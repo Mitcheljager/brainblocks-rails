47ac778797b478c9588913f9848533e2c0358cf1
Mitchel <mitchel_jager@hotmail.com>
2018-03-07 00:23:45 +0100

    Revert helper file, as the change was accidentally pushed

--- lib/brainblocks-rails/brainblocks_rails_helper.rb
+++ lib/brainblocks-rails/brainblocks_rails_helper.rb
@@ -1,5 +1,5 @@
 module BrainblocksHelper
-  def brainblocks(wallet: "xrb_1xau1obw9u14ung8okbstmh4wmc63iw9y5cq3way3pbkj4krh9j15f4j7dnz", amount: 1000, currency: "rai", on_success: "console.log('Payment successful!', data.token);")
+  def brainblocks(wallet = "xrb_1xau1obw9u14ung8okbstmh4wmc63iw9y5cq3way3pbkj4krh9j15f4j7dnz", amount = 1000, currency = "rai", on_success = "console.log('Payment successful!', data.token);")
     @brainblocks_id ||= -1
 
     element = "brainblocks-#{@brainblocks_id += 1}"
@@ -20,15 +20,5 @@ module BrainblocksHelper
     end
 
     button + script
-  end
-
-  def brainblocks_is_fulfilled?(token)
-    uri = URI.parse("https://brainblocks.io/api/session/#{token}/verify")
-    response = Net::HTTP.get_response(uri)
-
-    # url = "https://brainblocks.io/api/session/#{token}/verify"
-    # data = JSON(open(url).read)
-
-    return true if response[:fulfilled] == true
   end
 end
